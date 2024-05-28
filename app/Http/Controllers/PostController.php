12d50fb9685aa86861372b96086dde0a47f656ec
TOMO313 <AW4b13vd@icloud.com>
2024-05-28 23:09:23 +0900

    third commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use Inertia\Inertia;
+use Redirect;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -23,7 +25,7 @@ class PostController extends Controller
         return inertia("Post/Create");
     }
 
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
         $input = $request->all();
         $post->fill($input)->save();
